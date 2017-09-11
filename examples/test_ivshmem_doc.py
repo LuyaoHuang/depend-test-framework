77dc756f387537dc6480050c18cac57f8c2debea
Luyao Huang <lhuang@redhat.com>
2017-09-11 16:18:50 +0800

    examples: Add more test objects
    Signed-off-by: Luyao Huang <lhuang@redhat.com>

--- examples/test_ivshmem_doc.py
+++ examples/test_ivshmem_doc.py
@@ -11,6 +11,7 @@ def set_ivshmem_device(params, env):
     if params.ivshmem.model != 'ivshmem-plain':
         raise NotImplementedError
     params.doc_logger.info(STEPS + """
+    # cat ivshmem.xml
     <shmem name='%s'>
       <model type='%s'/>
       <size unit='KiB'>%d</size>
@@ -24,18 +25,29 @@ def set_ivshmem_device(params, env):
                   Provider('$guest_name.active.ivshmem', Provider.CLEAR)]
         end = [Provider('$guest_name.active.ivshmem', Provider.SET)]
 
+        start2 = [Provider('$guest_name.active', Provider.SET),
+                  Provider('$guest_name.active.ivshmem', Provider.CLEAR)]
+        end2 = [Provider('$guest_name.active.ivshmem', Provider.SET)]
+
         def use_invald_ivshmem_name(name, func, params, env):
             """
             Guest should fail to start with a invalid ivshmem device
             """
-            params.doc_logger.info(STEPS + "# virsh start %s", params.guest_name)
-            params.doc_logger.info(RESULT + """
-    error: Failed to start domain %s
-    error: unsupported configuration: shmem name '%s' must not contain '/'
-            """ % (params.guest_name, params.ivshmem.name))
+            if nane == 'start':
+                params.doc_logger.info(STEPS + "# virsh start %s", params.guest_name)
+                params.doc_logger.info(RESULT + """
+        error: Failed to start domain %s
+        error: unsupported configuration: shmem name '%s' must not contain '/'
+                """ % (params.guest_name, params.ivshmem.name))
+            elif name == 'attach':
+                params.doc_logger.info(STEPS + "# virsh attach-device %s ivshmem.xml --live", params.guest_name)
+                params.doc_logger.info(RESULT + """
+        error: Failed to attach device from ivshmem.xml
+        error: unsupported configuration: shmem name '%s' must not contain '/'
+                """ % (params.guest_name, params.ivshmem.name))
             raise MistDeadEndException
 
-        return Mist({"start": (start, end)}, use_invald_ivshmem_name)
+        return Mist({"start": (start, end), "attach": (start2, end2)}, use_invald_ivshmem_name)
 
 def check_ivshmem_cmdline(params, env):
     """
@@ -64,15 +76,15 @@ def check_ivshmem_audit(params, env):
 
     start1 = [Provider('$guest_name.active', Provider.SET),
              Provider('$guest_name.active.ivshmem', Provider.CLEAR)]
-    end1 = [Provider('$guest_name.active', Provider.SET),
-            Provider('$guest_name.active.ivshmem', Provider.SET)]
+    end1 = [Provider('$guest_name.active.ivshmem', Provider.SET)]
 
     start2 = [Provider('$guest_name.active', Provider.CLEAR),
               Provider('$guest_name.active.ivshmem', Provider.CLEAR)]
     end2 = [Provider('$guest_name.active.ivshmem', Provider.SET)]
 
     start3 = [Provider('$guest_name.active.ivshmem', Provider.SET)]
-    end3 = [Provider('$guest_name.active.ivshmem', Provider.CLEAR)]
+    end3 = [Provider('$guest_name.active.ivshmem', Provider.CLEAR),
+            Provider('$guest_name.active', Provider.SET)]
 
     def check_audit_log(name, func, params, env):
         """
